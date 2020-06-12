864a66e6058c08b52215daf8062e5d205c572c78
VicThor03 <vicmiheev@gmail.com>
2020-06-12 23:16:25 +0200

    Added Player's movement

--- 2DShooterGame/Assets/Scripts/PlayerManager.cs
+++ 2DShooterGame/Assets/Scripts/PlayerManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector3 m = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        m.z = 1.0f;
+       Vector3 c = m - transform.position;
+       transform.position = c + transform.position;
     }
 }
