caa9bcd58a0a8fe9d27266a6774b091e98ca582c
Dom <dom0216sal@outlook.it>
2020-06-13 13:06:14 +0200

    Added look at rot for the player on the mouse

--- 2DShooterGame/Assets/Scripts/PlayerManager.cs
+++ 2DShooterGame/Assets/Scripts/PlayerManager.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 m = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        m.z = 1.0f;
-       Vector3 c = m - transform.position;
-       transform.position = c + transform.position;
+        Vector3 m = Camera.main.ScreenToWorldPoint(Input.mousePosition);  //Get mouse pos and convert from screen to world space
+        m.z = 1.0f; //Set the Z pos at 1 to stay the sprite on top of the background
+        Vector3 dir = m - transform.position; // Get direction from player pos to mouse pos
+        
+        transform.position += dir; // Teleport player to mouse pos
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg; // Calculate the angle to apply on the Z axis to look at the mouse
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); // Rotate the player to mouse pos
     }
 }
